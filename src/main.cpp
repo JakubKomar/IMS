0f2db93dd8fed77e929b7ec7f88fe303bc796160
komca12 <57370991+komca12@users.noreply.github.com>
2021-12-07 20:45:44 +0100

    dfgvfsx

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,38 @@
 #include "main.hpp"
+#include <stdlib.h>
+using namespace std;
 
-int main()
+
+class Ship : public Process{
+    void Behavior()
+    {
+
+    }
+};
+
+class HarborUS : public Process{
+    void Behavior()
+    {
+
+    }
+};
+
+class HarborDE : public Process{
+    void Behavior()
+    {
+
+    }
+};
+
+
+int main(int argc, char** argv)
 {
-    std::cout<<"hallo word\n";
+    for(;;)
+    {
+        
+    }
+
+
+    cout<<"hallo word\n";
     return 0;
 }
